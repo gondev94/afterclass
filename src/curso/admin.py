d864ae857f095fa8455e9f277903a11dfd39e6ce
Gonzalo Peralta <gonnzaperalta94@gmail.com>
2024-12-08 18:56:28 +0100

    Modelos Listos!

--- src/curso/admin.py
+++ src/curso/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Comision, Curso, Alumno
 
-# Register your models here.
+@admin.register(Comision)
+class ComisionAdmin(admin.ModelAdmin):
+    list_display = ("curso__nombre", "numero", "fecha_inicio")
+
+@admin.register(Curso)
+class CursoAdmin(admin.ModelAdmin):
+    list_display = ("nombre", )
+
+@admin.register(Alumno)
+class AlumnoAdmin(admin.ModelAdmin):
+    list_display = ("Comision", "dni")
--- src/curso/models.py
+++ src/curso/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+
+class Curso(models.Model):
+    nombre = models.CharField(max_length=255, unique=True)
+    def __str__(self):
+        return self.nombre
+class Comision(models.Model):
+    curso =models.ForeignKey(Curso, on_delete=models.SET_NULL, null=True)
+    numero =models.PositiveIntegerField()
+    fecha_inicio =models.DateField()
+    def __str__(self):
+        return f"{self.curso} - {self.numero}"
+    
+class Alumno(models.Model):
+    Comision =models.ForeignKey(Comision, on_delete=models.SET_NULL, null=True)
+    dni = models.IntegerField()
+    
+
